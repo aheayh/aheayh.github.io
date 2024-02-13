477f76c64094b037dbda043f7a6805a5dfcca1c4
aheayh <aheayh@163.com>
2024-02-13 17:30:43 +0800

    Site updated: 2024-02-13 17:30:42

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2023/05/12/Ubuntu桌面学习/","2023/07/12/Markdown语法/"];function toRandomPost(){
+var posts=["2023/08/28/谈谈ChatGPT/","2023/05/12/Ubuntu桌面学习/","2023/07/12/Markdown语法/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
