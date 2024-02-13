374b5e7d3afa3f9237400df0a47f9c0dcb3089a9
aheayh <aheayh@163.com>
2024-02-13 09:33:26 +0800

    Site updated: 2024-02-13 09:33:26

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/02/12/hello-world/","2024/02/12/123/"];function toRandomPost(){
+var posts=["2024/02/12/123/","2024/02/12/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
